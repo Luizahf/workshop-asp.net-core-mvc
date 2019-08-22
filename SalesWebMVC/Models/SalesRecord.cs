dcb83e36bd428b2d30fd82bef3e69ff783ed75ff
Luizahf <luizahfreire@hotmail.com>
2019-08-22 14:54:18 -0300

    Implementing simple and grouping searches

--- SalesWebMVC/Models/SalesRecord.cs
+++ SalesWebMVC/Models/SalesRecord.cs
@@ -1,28 +1,17 @@
 ﻿using SalesWebMVC.Models.Enums;
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace SalesWebMVC.Models
 {
     public class SalesRecord
     {
-        public SalesRecord()
-        { }
-
-        public SalesRecord(int id, DateTime date, double amount, SaleStatus status, Seller seller)
-        {
-            Id = id;
-            Date = date;
-            Amount = amount;
-            Status = status;
-            Seller = seller;
-        }
-
-        public SalesRecord(int v1, DateTime dateTime, double v2, object saleStatus, Seller s1)
-        {
-        }
-
         public int Id { get; set; }
+
+        [DisplayFormat(DataFormatString ="{0:dd/MM/yyyy}")]
         public DateTime Date { get; set; }
+
+        [DisplayFormat(DataFormatString ="{:F2}")]
         public double Amount { get; set; }
         public SaleStatus Status { get; set; }
         public Seller Seller { get; set; }
